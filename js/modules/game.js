e7d2055bd77cf2fcd3babfb6e7d7c1d417eb72eb
Darin Hajou <it_related@hotmail.com>
2023-02-02 21:26:16 +0100

    Created HTML in js and added classlit

--- js/modules/game.js
+++ js/modules/game.js
@@ -1,6 +1,6 @@
 export default function () {
 
-	// Grabbing a couple of elements
+	// Grabbing a couple of elements from the DOM
 	const section = document.querySelector('section');
 	const playerLivesCount = document.querySelector('span');
 	const playerLives = 6;
@@ -10,32 +10,49 @@ export default function () {
 
 		// Generate data for the cards
 		const getData = () => [
-			{imgSrc: '../assets/images/peppa.jpg', name: "cat"},
+			{imgSrc: '../assets/images/peppa.jpg', name: "peppa"},
 			{imgSrc: '../assets/images/cat.jpg', name: "cat"},
-			{imgSrc: '../assets/images/dog.jpg', name: "cat"},
-			{imgSrc: '../assets/images/pappapig.jpg', name: "cat"},
-			{imgSrc: '../assets/images/pedro.jpg', name: "cat"},
-			{imgSrc: '../assets/images/mama.jpg', name: "cat"},
-			{imgSrc: '../assets/images/george.jpg', name: "cat"},
-			{imgSrc: '../assets/images/zebra.jpg', name: "cat"},
-			{imgSrc: '../assets/images/peppa.jpg', name: "cat"},
+			{imgSrc: '../assets/images/dog.jpg', name: "dog"},
+			{imgSrc: '../assets/images/pappapig.jpg', name: "pappapig"},
+			{imgSrc: '../assets/images/pedro.jpg', name: "pedro"},
+			{imgSrc: '../assets/images/mama.jpg', name: "mama"},
+			{imgSrc: '../assets/images/george.jpg', name: "george"},
+			{imgSrc: '../assets/images/zebra.jpg', name: "zebra"},
+			{imgSrc: '../assets/images/peppa.jpg', name: "peppa"},
 			{imgSrc: '../assets/images/cat.jpg', name: "cat"},
-			{imgSrc: '../assets/images/dog.jpg', name: "cat"},
-			{imgSrc: '../assets/images/pappapig.jpg', name: "cat"},
-			{imgSrc: '../assets/images/pedro.jpg', name: "cat"},
-			{imgSrc: '../assets/images/mama.jpg', name: "cat"},
-			{imgSrc: '../assets/images/george.jpg', name: "cat"},
-			{imgSrc: '../assets/images/zebra.jpg', name: "cat"},
+			{imgSrc: '../assets/images/dog.jpg', name: "dog"},
+			{imgSrc: '../assets/images/pappapig.jpg', name: "pappapig"},
+			{imgSrc: '../assets/images/pedro.jpg', name: "pedro"},
+			{imgSrc: '../assets/images/mama.jpg', name: "mama"},
+			{imgSrc: '../assets/images/george.jpg', name: "george"},
+			{imgSrc: '../assets/images/zebra.jpg', name: "zebra"},
+		
 			
 		];
 
-		// Running the index randomized 
+		// Running randomizing the index 
 		const shuffleCards = () => {
 			const cardData = getData();
 			cardData.sort(() => Math.random() - 0.5);
-			console.log(cardData);
+			return cardData;
 		};
 
-		shuffleCards();
-}
+		// Card generator function
+		const cardGenerator = () => {
+			const cardData = shuffleCards();
+			// Generate the HTML
+			cardData.forEach(item => {
+				console.log(item);
+				const card = document.createElement('div');
+				const face = document.createElement('img');
+				const back = document.createElement('div');
+				card.classList = 'card';
+				face.classList = 'face';
+				back.classList = 'back';
+			});
+		};
+
+		cardGenerator();
+
+};
 
